b420a6a1b0e6749473ee223f78d9d350af82dc17
kurakura0147 <tabatayuuta@tabatayuutanoMacBook-Pro.local>
2019-01-14 13:03:22 +0900

    Edit for test + view

--- chat-space/app/controllers/messages_controller.rb
+++ chat-space/app/controllers/messages_controller.rb
@@ -1,5 +1,4 @@
 class MessagesController < ApplicationController
-
   before_action :set_group
 
   def index
@@ -21,12 +20,10 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:message).permit(:body, :image).merge(user_id: current_user.id)
   end
 
   def set_group
     @group = Group.find(params[:group_id])
   end
-
-
 end
